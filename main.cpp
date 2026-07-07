f603fc1caf6c87d9692bc6294bc120cba09e0598
XT <3348379832@qq.com>
2026-07-07 20:05:55 +0800

    Squashed commit of the following:
    commit e6fa4989892fd8c0fc3326c1c4f23642e8b5d447
    Author: XT <3348379832@qq.com>
    Date:   Tue Jul 7 19:55:39 2026 +0800
    
        11
    
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include<iostram>
 void a() {}
-<<<<<<< HEAD
+
 void b() {}
 
-=======
+
 
 void c() {}
->>>>>>> remotes/origin/aaa
+
 int main(void)
 {
 	std::cout << "hello world 11111" << std::endl;
